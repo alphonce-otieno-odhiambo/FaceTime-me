fe7e206ee829ee68cf1f0f4abbf17783accd48d0
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-06-12 18:59:43 +0300

    create functionality to add and delete a vide

--- static/js/streams.js
+++ static/js/streams.js
@@ -8,24 +8,25 @@ let remoteUsers = {}
 
 let joinAndDisplayLocalStream = async () => {
     client.on('user-published', handleUserJoined)
+    client.on('user-left', handleUserLeft)
 
     UID =  await client.join(APP_ID, CHANNEL, TOKEN, null)
 
     localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
 
-    let player = `div class="video-container" id="user-container-${UID}">
-                    <div class="username-wrapper"><span class="user-name"></span>My name</div>
+    let player = `
+                div class="video-container" id="user-container-${UID}">
+                    <div class="username-wrapper"><span class="user-name"></span>My name
+                    </div>
                     <div class="video-player" id="user-${UID}">
                     </div>
-                </div>`
+                </div>
+                `
     document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
 
     localTracks[1].play(`user-${UID}`)
     await client.publish([localTracks[0], localTracks[1]])
 }
-
-
-
 
 
 let handleUserJoined = async (user, mediaType) => {
@@ -52,4 +53,49 @@ let handleUserJoined = async (user, mediaType) => {
     }
 
 }
+
+
+
+let handleUserLeft = async (user) => {
+    delete remoteUsers[user.uid]
+    document.getElementById(`user-container-${user.uid}`).remove()
+}
+
+
+let leaveAndRemoveLocalStream = async () => {
+    for (let i=0; localTracks.length > i; i++){
+        localTracks[i].stop()
+        localTracks[i].close()
+    }
+    await client.leave()
+    //This is somewhat of an issue because if user leaves without actaull pressing leave button, it will not trigger
+    // deleteMember()
+    window.open('/', '_self')
+}
+
+let toggleCamera = async (e) => {
+    if(localTracks[1].muted){
+        await localTracks[1].setMuted(false)
+        e.target.style.background = '#fff'
+    }else{
+        await localTracks[1].setMuted(true)
+        e.target.style.background = 'rgb(255, 80, 80,1)'
+    }
+}
+
+
+let toggleMic = async (e) => {
+    if(localTracks[0].muted){
+        await localTracks[1].setMuted(false)
+        e.target.style.background = '#fff'
+    }else{
+        await localTracks[0].setMuted(true)
+        e.target.style.background = 'rgb(255, 80, 80,1)'
+    }
+}
+
+
 joinAndDisplayLocalStream();
+document.getElementById('leave-btn').addEventListener('click', leaveAndRemoveLocalStream )
+document.getElementById('camera-btn').addEventListener('click', toggleCamera )
+document.getElementById('mic-btn').addEventListener('click', toggleMic )
